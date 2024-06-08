2a6575a5e0092ea0232956e2bb73aab28f2c77ca
eloy <eloysandoval10@hotmail.com>
2024-06-07 19:33:25 -0500

    save changes

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,32 +1,43 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from "react-router-dom"
-import "./styles/Navbar.css"
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./styles/Navbar.css";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState(false)
+  const [menu, setMenu] = useState(false);
 
-  const { products } = useSelector(store => store.cart)
-  const { token } = useSelector(store => store.userInfo)
+  const { products } = useSelector((store) => store.cart);
+  const { token } = useSelector((store) => store.userInfo);
 
   const toggleMenu = () => {
-    setMenu(!menu)
-  }
+    setMenu(!menu);
+  };
   const close = () => {
-    setMenu(!menu)
-  }
-  
-  return (
-    <nav className='navbar'>
-        <Link className='navbar__name' to="/"><h2>e-commerce</h2></Link>
-        <button onClick={toggleMenu} className='menuH'><i className=' bx bx-menu'></i></button>
-        <div className={`navbar__containerLinks ${ menu ? 'active' : ''}`}>
-            <Link onClick={close} className='navbar__link ' to="/login"><i className='bx bx-user'></i></Link>
-            <Link onClick={close} className='navbar__link ' to="/purchases"><i className='bx bx-box'></i></Link> 
-            <Link onClick={close}  className='navbar__link' to="/cart">{token ? <span>{products?.length}</span> : ""}<i  className='bx bx-cart'></i></Link>
-        </div>
-    </nav>
-  )
-}
+    setMenu(!menu);
+  };
 
-export default Navbar
+  return (
+    <nav className="navbar">
+      <Link className="navbar__name" to="/">
+        <h2>e-commerce</h2>
+      </Link>
+      <button onClick={toggleMenu} className="menuH">
+        <i className=" bx bx-menu"></i>
+      </button>
+      <div className={`navbar__containerLinks ${menu ? "active" : ""}`}>
+        <Link onClick={close} className="navbar__link " to="/login">
+          <i className="bx bx-user"></i>
+        </Link>
+        <Link onClick={close} className="navbar__link " to="/purchases">
+          <i className="bx bx-box"></i>
+        </Link>
+        <Link onClick={close} className="navbar__link" to="/cart">
+          {token ? <span>{products?.length}</span> : ""}
+          <i className="bx bx-cart"></i>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
